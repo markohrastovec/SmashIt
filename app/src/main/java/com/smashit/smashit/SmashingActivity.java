b8bdb7446e3958f7943d6b72c1e14b0345a4bda0
Marko Hrastovec <marko.hrastovec@gmail.com>
2014-11-30 21:20:51 +0100

    Learning SurfaceView.

--- app/src/main/java/com/smashit/smashit/SmashingActivity.java
+++ app/src/main/java/com/smashit/smashit/SmashingActivity.java
@@ -3,21 +3,12 @@ package com.smashit.smashit;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.smashit.smashit.util.SystemUiHider;
 
-
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class SmashingActivity extends Activity {
 
   @Override
   protected void onCreate (Bundle savedInstanceState) {
     super.onCreate (savedInstanceState);
-
     setContentView (R.layout.activity_smashing);
   }
 }
---
+++ app/src/main/java/com/smashit/smashit/SmashingView.java
@@ -0,0 +1,136 @@
+package com.smashit.smashit;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.smashit.smashit.R;
+
+import java.util.jar.Attributes;
+
+/**
+ * View for drawing the smashing on screen
+ */
+public class SmashingView extends SurfaceView implements SurfaceHolder.Callback {
+  private Bitmap originalBackground;
+  private SmashingThread smashingThread;
+  private SurfaceHolder surfaceHolder;
+  Context context;
+
+  public SmashingView (Context ctxt, AttributeSet attrs) {
+    super (ctxt, attrs);
+    context = ctxt;
+    surfaceHolder = getHolder ();
+    surfaceHolder.addCallback (this);
+    //originalBackground = decodeSampledBitmapFromResource (getResources (), R.drawable.bricks, this.getWidth (), this.getHeight ());
+    originalBackground = BitmapFactory.decodeResource (getResources (), R.drawable.bricks);
+  }
+
+//  @Override
+//  protected void onDraw (Canvas canvas) {
+//    canvas.drawBitmap (originalBackground, 0, 0, null);
+//  }
+
+  public static int calculateInSampleSize (BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    // Raw height and width of image
+    final int height = options.outHeight;
+    final int width = options.outWidth;
+    int inSampleSize = 1;
+
+    if (height > reqHeight || width > reqWidth) {
+
+      final int halfHeight = height / 2;
+      final int halfWidth = width / 2;
+
+      // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+      // height and width larger than the requested height and width.
+      while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
+        inSampleSize *= 2;
+      }
+    }
+
+    return inSampleSize;
+  }
+
+  public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+    // First decode with inJustDecodeBounds=true to check dimensions
+    final BitmapFactory.Options options = new BitmapFactory.Options ();
+    options.inJustDecodeBounds = true;
+    BitmapFactory.decodeResource (res, resId, options);
+
+    // Calculate inSampleSize
+    options.inSampleSize = calculateInSampleSize (options, reqWidth, reqHeight);
+
+    // Decode bitmap with inSampleSize set
+    options.inJustDecodeBounds = false;
+    return BitmapFactory.decodeResource (res, resId, options);
+  }
+
+  @Override
+  public void surfaceCreated (SurfaceHolder holder) {
+    smashingThread = new SmashingThread (holder, context);
+    smashingThread.setRunning (true);
+    smashingThread.start ();
+  }
+
+  @Override
+  public void surfaceChanged (SurfaceHolder holder, int format, int width, int height) {
+  }
+
+  @Override
+  public void surfaceDestroyed (SurfaceHolder holder) {
+    smashingThread.setRunning (false);
+    boolean retry = true;
+    while (retry) {
+      try {
+        smashingThread.join ();
+        retry = false;
+      }
+      catch (Exception e)
+      {
+        Log.v ("Exception occurred", e.getMessage ());
+      }
+    }
+  }
+
+  void doDraw (Canvas canvas) {
+    canvas.drawBitmap (originalBackground, 0, 0, null);
+  }
+
+  public class SmashingThread extends Thread {
+    boolean running;
+    Canvas canvas;
+    SurfaceHolder surfaceHolder;
+    Context context;
+
+    public SmashingThread (SurfaceHolder sHolder, Context ctx)
+    {
+      surfaceHolder = sHolder;
+      context = ctx;
+      running = false;
+    }
+
+    void setRunning (boolean bRun)
+    {
+      running = bRun;
+    }
+
+    @Override
+    public void run() {
+      super.run ();
+      while (running) {
+        canvas = surfaceHolder.lockCanvas ();
+        if (canvas != null) {
+          SmashingView.this.doDraw (canvas);
+          surfaceHolder.unlockCanvasAndPost (canvas);
+        }
+      }
+    }
+  }
+}
